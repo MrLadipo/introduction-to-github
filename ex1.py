b93ba5582cfcd14916cc7a3ef1888e48901ea609
MrLadipo <74557296+MrLadipo@users.noreply.github.com>
2023-09-28 23:51:27 +0100

    Created a Python File

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# Create student data
+
+# student_name = input("Enter your student name: ")
+
+student_class = []
+
+for i in range(3):
+    student_class.apppend(input("Enter class name:"))
+
+print(student_class)
